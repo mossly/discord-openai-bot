f2ad368e7b3750c2ab8635b19db1a558e986f585
mossly <aaron.mozz@gmail.com>
2024-04-17 15:18:40 +1200

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -36,7 +36,7 @@ def background():
                 print(f'Time now: {convert_to_readable(now)} | Reminder time: {convert_to_readable(timestamp)}')
                 if timestamp < now:
                     # simulate user action
-                    user = client.fetch_user("Mossly")
+                    user = client.fetch_user("195485849952059392")
                     user.send(f'Sent reminder: {reminders2[timestamp]}')
                     del reminders2[timestamp]
                     reminder_times.remove(timestamp)  # remove the processed timestamp
