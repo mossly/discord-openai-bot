c42d972e453211d2038f2d7b672ff77fc1a34dbe
Mossly <moss.aaron@icloud.com>
2025-02-03 11:43:03 +1300

    async main

--- discordbot.py
+++ discordbot.py
@@ -1,16 +1,19 @@
 import os
 import discord
 from discord.ext import commands
+import asyncio
 
 intents = discord.Intents.default()
+intents.message_content = True
+
 bot = commands.Bot(command_prefix="!", intents=intents)
 
-for filename in os.listdir("./cogs"):
-    if filename.endswith(".py"):
-        bot.load_extension(f"cogs.{filename[:-3]}")
+async def main():
+    for filename in os.listdir("./cogs"):
+        if filename.endswith(".py"):
+            await bot.load_extension(f"cogs.{filename[:-3]}")
+    
+    async with bot:
+        await bot.start(os.getenv("BOT_API_TOKEN"))
 
-@bot.event
-async def on_ready():
-    print(f"Logged in as {bot.user}!")
-
-bot.run(os.getenv("BOT_API_TOKEN"))
+asyncio.run(main())
